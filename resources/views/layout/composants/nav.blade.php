9f45f036ac7603ddbac8a965b7f4f82508a3d348
adel el amrani <fanasolo@hotmail.com>
2018-06-21 09:32:02 +0200

    premier commit

---
+++ resources/views/layout/composants/nav.blade.php
@@ -0,0 +1,28 @@
+<!-- Page Preloder -->
+@extends('layout.')
+<div id="preloder">
+    <div class="loader">
+        <img src="img/logo.png" alt="">
+        <h2>Loading.....</h2>
+    </div>
+</div>
+
+
+<!-- Header section -->
+<header class="header-section">
+    <div class="logo">
+        <img src="img/logo.png" alt=""><!-- Logo -->
+    </div>
+    <!-- Navigation -->
+    <div class="responsive"><i class="fa fa-bars"></i></div>
+    <nav>
+        <ul class="menu-list">
+            <li class="active"><a href="home.html">Home</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="elements.html">Elements</a></li>
+        </ul>
+    </nav>
+</header>
+<!-- Header section end -->
